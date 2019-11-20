931328471a55a57c905db3eed914d5c63e26548f
Joshua Fortune <4gatortm@gmail.com>
2019-11-20 10:55:20 -0600

    build: rebasing master

--- smpp/src/app/app.module.ts
+++ smpp/src/app/app.module.ts
@@ -1,7 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
 import { LayoutModule } from '@angular/cdk/layout';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
   MatButtonModule,
@@ -10,9 +8,10 @@ import {
   MatIconModule,
   MatInputModule,
   MatSelectModule,
-  MatToolbarModule
+  MatToolbarModule,
 } from '@angular/material';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GoogleChartsModule } from 'angular-google-charts';
 
 import { AppComponent } from './app.component';
@@ -20,12 +19,7 @@ import { NnInputComponent } from './nn-input/nn-input.component';
 import { StockHistoryGraphComponent } from './nn-input/stock-history-graph/stock-history-graph.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NnInputComponent,
-    StockGraphComponent,
-    EsOutputComponent
-  ],
+  declarations: [AppComponent, NnInputComponent, StockHistoryGraphComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
